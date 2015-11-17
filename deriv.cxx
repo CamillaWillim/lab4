67b5f23cfb786d23a4bf4cbf8f16bc46bd49ba70
Camilla Willim <cwillim@pc09.pool.tp1.uni-duesseldorf.de>
2015-11-17 15:28:48 +0100

    HAHAHA

--- deriv.cxx
+++ deriv.cxx
@@ -3,13 +3,9 @@
 
 using namespace std;
 
-
-void print(double* p, const int N, const double dx,
-           const double xmin)
-{
-   for(int i=0; i<N; i++)
-       cout << i*dx + xmin << "\t" << p[i] << endl;
-}
+void fillarray(int N, const double xmax, const double xmin, const double dx, double* f);
+void print(double* p, const int N, const double dx,const double xmin);
+void deriva(int N, const double dx, double* f);
 
 
 int main(){
@@ -19,9 +15,39 @@ int main(){
   const double xmin = -15;
   const double dx = (xmax-xmin)/(N-1);
 
-  // call to function which fills array p here
-  // call to functio which calculates the derivative
+  fillarray(N,xmax,xmin,dx,p);
+  deriva(N,dx,p);
   print(p,N,dx,xmin);
+  
 
   return 0;
 }
+
+void print(double* p, const int N, const double dx,
+           const double xmin)
+{
+   for(int i=0; i<N; i++)
+       cout << i*dx + xmin << "\t" << p[i] << endl;
+}
+
+void fillarray(int N, const double xmax, const double xmin, const double dx, double* f){
+  double x=xmin;
+  for(int i=0; i<N; i++){
+    f[i]=exp(-pow(x,2));
+    x+=dx;
+  }
+}
+
+void deriva(int N, const double dx, double* f){
+  double falt;
+  double fs;
+  f[0]=f[1]/(2*dx);
+  f[N-1]=-f[N-2]/(2*dx);
+  falt = f[0];
+  for(int i=1; i<N-1; i++){
+    fs=(f[i+1]-falt)/(2*dx);
+    falt = f[i];
+    f[i]=fs;
+  }
+}
+    
